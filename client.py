6635cae3d3a8dd8b57e475544bc0ed279d271e47
Fan Zhang <farlenzhang@gmail.com>
2017-11-23 14:20:27 -0800

    Initial implementation of client crash handling

--- client.py
+++ client.py
@@ -2,27 +2,41 @@ import sys
 import socket
 import select
 import string
+import Queue
+import pdb
+
 
 buf_size = 1024
 
 def main():
     client = Client()
-    client.getCmd()
+    client.get_cmd()
 
 class Client:
     def __init__(self):
         self.port = 8080
         self.host = 'localhost'
         self.quit = False
+        self.reqs_queue = Queue.Queue()
 		# Attempt to connect to server
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.connect((self.host, self.port))
-            sys.stdout.write('Successfully Connected to Server\n')
+            print('Successfully Connected to Server\n')
         except:
-            sys.stdout.write('Failed to Connect to Server')
-            
-    def getCmd(self):
+            print('Failed to Connect to Server\n')
+         
+    def enqueue_reqs(self, reqs):
+        self.reqs_queue.put(reqs)
+        
+    def dequeue_reqs(self):
+        try :
+            return self.reqs_queue.get()
+        except Queue.Empty:
+            print("Numbers of Requests don't match with responses\n")
+        
+                
+    def get_cmd(self):
         while not self.quit:
             try:
                 readable, writable, errored = select.select([0, self.socket], [],[])
@@ -31,23 +45,38 @@ class Client:
                         # read user input from stdin
                         cmd = sys.stdin.readline().strip()
                         reqs = cmd.split(" ")
-                        req = self.checkReq(reqs[0])
+                        req = self.check_req(reqs)
         
                         if req is 'INVALID':
-                            sys.stdout.write("Invalid command.\n")
+                            print("Invalid command.\n")
+                        elif req is 'WRONG_ARGS':
+                            print("Wrong number of arguments for \"" + reqs[0] + "\"\n")
                         elif req is 'QUIT':
                            # client mark itself as "quit"
                            # also sends request "QUIT" to server
                            self.quit = True
-                           self.socket.send(cmd)
-                           resp = self.socket.recv(buf_size)
-                           self.parseResponse(reqs, resp) 
+                           self.socket.send(cmd + '\n')
+                           self.enqueue_reqs(reqs)
+                           #resp = self.socket.recv(buf_size)
+                           #self.parse_response(reqs, resp) 
+                        elif req is 'PFILE':
+                            fname = reqs[2]
+                            fp = open(fname, 'rb')
+                            if not fp:
+                                print("File does not exist.\n")
+                            else:
+                                self.socket.send(cmd)
+                                resp = self.socket.recv(buf_size)
+                                self.parse_response(reqs, resp)
+                                if resp == 'OK_PFILE':
+                                    self.send_file(fp)
+                                    print("\nFile transferred\n")
+                            self.reqs_queue.put(reqs)
                         else:
                             # valid command, forward to server
-                            self.socket.send(cmd)
-                            # receive and parse the response from server
-                            resp = self.socket.recv(buf_size)
-                            self.parseResponse(reqs, resp);
+                            self.socket.send(cmd + '\n')
+                            self.reqs_queue.put(reqs)
+                            
                     else:
                         # conn == self.socket
                         # got message from one room from server, display it directly
@@ -56,100 +85,177 @@ class Client:
                         if not msg:
                             sys.exit("Lost connection to server.\n")
                             
-                        self.parseServerMsg(msg);
+                        #self.parse_server_msg(msg);
+                        self.parse_multi_msg(msg)
+                        #print("queue: ", repr(self.reqs_queue))
+            
             except KeyboardInterrupt:
+                self.socket.send('QUIT\n')
+                self.enqueue_reqs(['QUIT'])
                 self.quit = True
-                sys.stdout.write('Keyboard Interrupt. Disconnected. \n')
+                print('Keyboard Interrupt. Disconnected. \n')
                 self.socket.close()
-                break
-                
-    def parseServerMsg(self, msg):
-        parsed = msg.strip().split(" ")
-        type = parsed[0]    
-        # type could either be MESSAGE or PING
-        if type == 'PING':
-            self.socket.send('PONG')
-        elif type == 'MESSAGE':
-            sys.stdout.write("Message from " + parsed[-1] + " in room " + parsed[1] + ": " + ' '.join(parsed[2:-1]) + "\n")
-            sys.stdout.flush()
-        else:
-            sys.stdout.write("Unparsed message from server:\n " + msg + "\n") 
+                break        
                         
-    def parseResponse(self, reqs, resp):
-        parsed = resp.strip().split(" ")
-        status = parsed[0]     
+    def parse_multi_msg(self, msg):
+        multi_msg = msg.strip().split("\n")
+        for message in multi_msg:
+            self.parse_msg(message)
+    
+    def parse_msg(self, msg):
+        parsed = msg.strip().split(" ")
+        status = parsed[0]   
         # response about register
-        if status == 'OK_REG':
-            sys.stdout.write("Registered successfully.\n")                    
-        elif status == 'ERR_USER_NAME_TAKEN':
-            sys.stdout.write("Name taken.\n")  
+        # type could either be MESSAGE or PING
+        if status == 'PING':
+            self.socket.send('PONG\n')
+        elif status == 'NOTICE':
+            print(' '.join(parsed[1:]) + "\n")
+            sys.stdout.flush()
+        elif status == 'MESSAGE':
+            print("Message from " + parsed[1] + " in room " + parsed[2]+ ": " + ' '.join(parsed[3:]) + "\n")
+            # MESSAGE lunch_room hello everyone mark17
+            sys.stdout.flush()
+        elif status == 'PMESSAGE':
+            print("Message from " + parsed[1] + " user " + parsed[2] + ": " + ' '.join(parsed[3:]) + "\n")
+            sys.stdout.flush()
+
+        elif status =='PFILE':
+            fname = parsed[3]
+            print("Receiving file from " + parsed[1] + " user " + parsed[2] + ": " + fname + " ...")
+            self.receive_file(fname)
+            print("File received.\n")
+            
+        elif status == 'OK_REG':
+            reqs = self.dequeue_reqs()
+            print("Registered successfully.\n")
+        elif status == 'ERR_USERNAME_TAKEN':
+            reqs = self.dequeue_reqs()
+            print("Name taken.\n") 
         elif status == 'ERR_USER_NOT_EXIST':
             # this resp can come from many requests when the client tries to create/join/leave/message
             # before register
-            sys.stdout.write("You need to register first.\n")       
+            reqs = self.dequeue_reqs()
+            print("You need to register first.\n")
         # response about creating a new room 
         elif status == 'OK_CREATE_ROOM':
-            sys.stdout.write("Room " + reqs[1] + " created.\n") 
+            reqs = self.dequeue_reqs()
+            print("Room " + reqs[1] + " created.\n")
         elif status == 'ERR_ALREADY_IN_ROOM':
-            sys.stdout.write("You are already in room " + reqs[1] + ".\n") 
+            reqs = self.dequeue_reqs()
+            print("You are already in room " + reqs[1] + ".\n")
+
         elif status == 'ERR_ROOM_NAME_TAKEN':
-            sys.stdout.write("Room name " + reqs[1] + " is taken.\n")  
+            reqs = self.dequeue_reqs()
+            print("Room name " + reqs[1] + " is taken.\n")
                             
         # response about joining a room
         elif status == 'OK_JOIN_ROOM':
-            sys.stdout.write("Joined room " + reqs[1] + ".\n") 
+            reqs = self.dequeue_reqs()
+            print("Joined room " + reqs[1] + ".\n")
+
         elif status == 'ERR_ALREADY_IN_ROOM':
-            sys.stdout.write("You are already in room " + reqs[1] + ".\n") 
+            reqs = self.dequeue_reqs()
+            print("You are already in room " + reqs[1] + ".\n")
+
         elif status == 'ERR_ROOM_NOT_EXIST':
-            sys.stdout.write("There is no room named " + reqs[1] + ".\n") 
-                         
+            reqs = self.dequeue_reqs()
+            print("There is no room named " + reqs[1] + ".\n")
+
         # response about leaving a room
         elif status == 'OK_LEAVE_ROOM':
-            sys.stdout.write("Left room " + reqs[1] + ".\n") 
+            reqs = self.dequeue_reqs()
+            print("Left room " + reqs[1] + ".\n")
+
         elif status == 'ERR_NOT_IN_ROOM':
-            sys.stdout.write("You are not in room " + reqs[1] + ".\n")                                                          
+            reqs = self.dequeue_reqs()
+            # also used as response to MESSAGE when the user is not in the room they are sending message to
+            print("You are not in room " + reqs[1] + ".\n")
+                                                       
         elif status == 'ERR_ROOM_NOT_EXIST':
-            sys.stdout.write("There is no room named " + reqs[1] + ".\n")  
-              
+            reqs = self.dequeue_reqs()
+            print("There is no room named " + reqs[1] + ".\n")
+
               
         # response about listing rooms
         elif status == 'OK_LIST':
+            reqs = self.dequeue_reqs()
             if len(parsed) == 1:
-                sys.stdout.write("No Available Rooms.\n")  
+                print("No Available Rooms.\n")  
             else:
-                sys.stdout.write("Available Rooms: " + ' '.join(parsed[1:]) + ".\n")  
+                print("Available Rooms: " + ' '.join(parsed[1:]) + ".\n")
+              
         # response about listing members of a room
         elif status == 'OK_MEMBERS':
+            reqs = self.dequeue_reqs()
             if len(parsed) == 1:
-                sys.stdout.write("This room is empty.\n")  
+                print("This room is empty.\n")  
             else:
-                sys.stdout.write("Members in room " + reqs[1] + ": " + ' '.join(parsed[1:])  + ".\n")
+                print("Members in room " + reqs[1] + ": " + ' '.join(parsed[1:])  + ".\n")
+
    
         # response about sending a message
         elif status == 'OK_MESSAGE':
-            sys.stdout.write("Message sent to room " + reqs[1] + ".\n")            
-        elif status == 'ERR_MESSAGE':
-            sys.stdout.write("Message failed to be sent to room " + reqs[1] + ".\n")
-        
-        # response about quit
-        elif resp == 'OK_QUIT':
-            sys.stdout.write("Disconnected.\n")                    
-        elif resp == 'ERR_QUIT':
-            sys.stdout.write("...\n")           
+            reqs = self.dequeue_reqs()
+            print("Message sent to room " + reqs[1] + ".\n")                        
             
-        #else:
-            #sys.stdout.write("Unparsed response from server: \n" + resp + "\n")
-            
-            
-    def checkReq(self, req):
-        # check if the request is valid
-        valid_cmds = ['REGISTER', 'JOIN', 'LEAVE', 'MESSAGE', 'QUIT', 'LIST', 'CREATE', 'LIST_MY_ROOMS', 'MEMBERS']
-        # return the request type of the cmd
-        if req in valid_cmds:
-            return req;
-        else:
-            return 'INVALID'            
+        # response about sending a private message
+        elif status == 'OK_PMESSAGE':
+            reqs = self.dequeue_reqs()
+            print("Message sent to user " + reqs[1] + ".\n")
 
+        elif status == 'ERR_PUSER_NOT_EXIST':
+            reqs = self.dequeue_reqs()
+            print("User " + reqs[1] + " does not exist.\n") 
+
+        # response about quit
+        elif status == 'OK_QUIT':
+            reqs = self.dequeue_reqs()
+            print("Disconnected.\n")
+        elif status == 'ERR_QUIT':
+            reqs = self.dequeue_reqs()
+            print("...\n")
+        elif status == 'OK_PFILE':
+            reqs = self.dequeue_reqs()
+            print("Sending file ...")
+        else:
+            print("Unparsed message from server:\n " + msg + "\n") 
+                        
+            
+    def check_req(self, reqs):
+        # check if the request is valid
+        valid_cmds = ['REGISTER', 'JOIN', 'LEAVE', 'MESSAGE', 'PMESSAGE','QUIT', 'LIST', 'CREATE', 'LIST_MY_ROOMS', 'MEMBERS', 'PFILE']
+        # return the request type of the cmd
+        if reqs[0] not in valid_cmds:
+            return 'INVALID'
+        elif ((reqs[0] == 'QUIT' or reqs[0] == 'LIST' or reqs[0] == 'LIST_MY_ROOMS') and len(reqs) != 1):
+            return 'WRONG_ARGS'
+        elif ((reqs[0] == 'REGISTER' or reqs[0] == 'JOIN' or reqs[0] == 'CREATE' or reqs[0] == 'MEMBERS' or reqs[0] == 'LEAVE') and len(reqs) != 2 ):  
+            return "WRONG_ARGS"
+        elif ((reqs[0] == 'MESSAGE' or reqs[0] == 'PMESSAGE' or reqs[0] == 'PFILE') and len(reqs) < 3):
+            # at least 3 args. Can be more than 3 because spaces are allowed in message body
+            return "WRONG_ARGS"        
+        else:
+            return reqs[0]
+        
+    def send_file(self, fp):
+        # read and send the file content
+        batch = fp.read(buf_size)
+        while batch:
+            self.socket.send(batch)
+            batch = fp.read(buf_size)
+        fp.close()
+        
+    def receive_file(self, fname):
+        with open(fname, 'wb') as fp:
+            # open a file with the specified name
+            while True:
+                data = self.socket.recv(buf_size)
+                if not data:
+                    break
+                fp.write(data)
+        fp.close()
+        
 if __name__ == '__main__':
     main()
 
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import select
 import socket
 import sys
+from threading import Thread
+from time import sleep
 
 # TODO:
 # 1. REMOVE EMPTY ROOM
@@ -10,7 +12,11 @@ import sys
 # 5. class Room
 # 6. extract validation of user and room
 # 7. use socket to get user name (in listMembers())
-# 8. error handling when no `info`
+# 8. If the user has already registered, and sends “REGISTER xx” again, 
+#    server should return some error code. Currently this behavior causes crash in server.
+# 9. Other members of the room should get noticed when a client has left
+#10. Check if the user is in the room before removing them from the room if they send “LEAVE xx”.
+
 
 #####################################################
 # User Class
@@ -20,6 +26,7 @@ class User:
         self.name = name
         self.socket = socket
         self.rooms = []  # a list of rooms the user is in
+        self.active = True  # if the user is still connected
 
     def get_name(self):
         return self.name
@@ -43,6 +50,18 @@ class User:
         else:
             self.rooms.remove(room)
             return "OK_LEAVE_ROOM"
+        
+    # mark the user as active
+    def mark_active(self):
+        self.active = True
+    
+    # mark the user as inactive
+    def mark_inactive(self):
+        self.active = False
+    
+    # check if the user is active
+    def check_active(self):
+        return self.active
 
 # User Class - end
 #####################################################
@@ -124,16 +143,21 @@ def list_members(room):
     print "OK_MEMBERS ", ' '.join(members)
     return ("OK_MEMBERS " + ' '.join(members))
  
-def send_message(room, socket):
+def send_message(room, socket, msg_lst):
     if room not in rooms:
         return "ERR_ROOM_NOT_EXIST"
 
+    # TODO: Check if user is in this room
     user = find_user(socket)
     if user:
-        return ("OK_MESSAGE " + user.get_name() + ' ' + room)
+        return ("OK_MESSAGE " + user.get_name() + ' ' + room + ' ' + ' '.join(msg_lst))
+        # the msg content is needed in the return value for server to send to other users in the room
     else:
         return "ERR_USER_NOT_EXIST"
 
+def quit(socket):
+    return "OK_QUIT"
+    
 # helper function
 def find_user(socket):
     for user in users:
@@ -144,13 +168,25 @@ def find_user(socket):
 # dispatch functions - end
 #####################################################
 
-
+# data could be multiple messages queueing in the buffer, deliminated by '\n'
+# Split these messages and store in a list, dispatch each of them, store the 
+# return values in aother list, and return the list.
+def dispatch_multi(data, socket):
+    ret = []
+    multi_data = data.strip().split("\n")
+    print multi_data
+    for dt in multi_data:
+        resp = dispatch(dt, socket)
+        if resp:
+            ret.append(resp)
+    return ret
+            
+        
 def dispatch(data, socket):
-    args = data.split(" ")
+    args = data.strip().split(" ")
     cmd = args[0]
     if len(args) > 1:
         info = args[1]
-    # TODO: error handling when `info` does not exist
 
     if cmd == "REGISTER":
         return register(info, socket)
@@ -165,10 +201,61 @@ def dispatch(data, socket):
     elif cmd == "MEMBERS":
         return list_members(info)
     elif cmd == "MESSAGE":
-        return send_message(info, socket)
+        msg_lst = args[2:]
+        return send_message(info, socket, msg_lst)
+    elif cmd == "QUIT":
+        return quit(socket)
+    elif cmd == "PONG":
+        print 'dispatch', cmd
+        # if receive a 'PONG' from some user, mark it as active
+        user = find_user(socket)
+        user.mark_active()
+        pass
     else:
+        print 'dispatch', cmd
         return "ERR_INVALID"
+    
+def ping_clients():
+    # iterate all clients and send 'PING' to everyone.
+    for user in users:
+        sckt = user.get_socket()
+        sckt.send('PING\n')
+        print 'ping_clients'
 
+        #resp = sckt.recv(size)
+    sleep(1)
+    for user in users:
+        if user.check_active() == False:
+            # if any user is inactive, it means no 'PONG' was received 
+            # last round from its socket. so client crashed
+            # handle client left situation 
+            handle_client_leave(user)               
+        # mark all the users as inactive for next round of PING
+        user.mark_inactive()
+
+# this function is called when a user sends "QUIT", 
+# or when a client is crashed 
+def handle_client_leave(user):
+    if user:
+        sckt = user.get_socket()
+        sckt.close()
+        # remove client from user list
+        users.remove(user)
+        
+        rms = user.get_rooms()
+        name = user.get_name()
+        for room in rms:
+            sckts = rooms[room]
+            # remove the client from this room
+            sckts.remove(sckt)
+
+            # send message to other members of this room
+            for s in sckts:
+                s.send('NOTICE user ' + name + ' has left\n')
+
+
+#####################################################
+# main function
 
 def main():
     host = ''
@@ -177,47 +264,71 @@ def main():
     size = 1024
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print "Server is running on port 8080"
+    server.setblocking(0)
     server.bind((host,port))
-    server.listen(5)
+    server.listen(backlog)
+    print "Server is running on port 8080"
     inputs = [server]
     running = 1
     while running:
+        ping_clients()
         readable, writable, exceptional = select.select(inputs,[],[])
         for s in readable:
             # if the socket is server socket
             # accept new client connection and add it to the connection list
             if s is server:
                 connection, client_address = s.accept()
+                #connection.setblocking(0)
                 inputs.append(connection)
+
             # if the socket is client socket
             # read data from the socket and process them
             else:
-                data = s.recv(size)
-                if data:
-                    response = dispatch(data, s)
-                    if response:
+                try:
+                    # used try except because if client closes the connection, 
+                    # s.recv() here will cause error.
+
+                    data = s.recv(size)
+                    responses = dispatch_multi(data, s)
+                    #if response:
+                    for response in responses:
                         resp = response.split(' ')
                         status = resp[0]
+                        # TODO: extract to a function
                         if status == "OK_MESSAGE":
-                            print data
-                            s.send(status)
-                            user = resp[1]
+                            s.send(status + "\n")
+                            
+                            # TODO: come up with a better variable name for "to_send", 
+                            # it includes username, roomname, and the message body
+                            to_send = ' '.join(resp[1:])
+
+                            #user = resp[1]
                             room = resp[2]
+                            print data
                             for sckt in rooms[room]:
-                                sckt.send(data + ' ' + user)
+                                #sckt.send(data + ' ' + user + '\n')
+                                sckt.send('MESSAGE ' + to_send + '\n')
+                        elif status == "OK_QUIT":
+                            s.send(status + "\n")
+                            handle_client_leave(find_user(s))
+                            inputs.remove(s)
+                            # TODO Inform related users about this user's quit
                         else:
-                            s.send(response)
+                            print response
+                            s.send(response +"\n")
                     # if no valid response for this request
                     # continue to the next client
-                    else:
-                        continue
-                else:
+                    # else:
+                    #    continue
+                except socket.error:
+                    handle_client_leave(find_user(s))
                     inputs.remove(s)
-                    s.close()
-
 
     server.close()
 
+# main function - end
+#####################################################
+
 if __name__ == '__main__':
     main()
+
