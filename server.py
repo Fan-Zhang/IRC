54892e9889bc4c79d9d4b238bffa75f8b5e5a7ca
Fan Zhang <farlenzhang@gmail.com>
2017-11-24 01:39:33 -0800

    Notice other room members when a client has crashed or left a room. More user and room validations on server. Add class Room on server.

--- server.py
+++ server.py
@@ -6,16 +6,12 @@ from time import sleep
 
 # TODO:
 # 1. REMOVE EMPTY ROOM
-# 2. PING - Server can gracefully handle client crashes
-# 3. Create another thread to ping the clients periodically
-# 4. QUIT
-# 5. class Room
-# 6. extract validation of user and room
-# 7. use socket to get user name (in listMembers())
-# 8. If the user has already registered, and sends “REGISTER xx” again, 
-#    server should return some error code. Currently this behavior causes crash in server.
-# 9. Other members of the room should get noticed when a client has left
-#10. Check if the user is in the room before removing them from the room if they send “LEAVE xx”.
+# 2. QUIT
+# 3. Other members of the room should get noticed when a client has left
+# 4. Check if the user is in the room before removing them from the room if they send "LEAVE xx".
+# 5. leave() method
+# 6. server not exit with keyboard interrupt
+# 7. notice room members when new member joins
 
 
 #####################################################
@@ -25,32 +21,21 @@ class User:
     def __init__(self, name, socket):
         self.name = name
         self.socket = socket
-        self.rooms = []  # a list of rooms the user is in
-        self.active = True  # if the user is still connected
+        self.active = True  # the user is connected
 
     def get_name(self):
         return self.name
 
     def get_rooms(self):
-        return self.rooms
+        rms = []
+        for room in rooms.itervalues():
+            if self.name in room.get_members():
+                rms.append(room.get_name())
+        return rms
 
     def get_socket(self):
         return self.socket
 
-    def join_room(self, room):
-        if room in self.rooms:
-            return "ERR_ALREADY_IN_ROOM"
-        else:
-            self.rooms.append(room)
-            return "OK_JOIN_ROOM"
-
-    def leave_room(self, room):
-        if room not in self.rooms:
-            return "ERR_NOT_IN_ROOM"
-        else:
-            self.rooms.remove(room)
-            return "OK_LEAVE_ROOM"
-        
     # mark the user as active
     def mark_active(self):
         self.active = True
@@ -60,7 +45,7 @@ class User:
         self.active = False
     
     # check if the user is active
-    def check_active(self):
+    def is_active(self):
         return self.active
 
 # User Class - end
@@ -68,10 +53,60 @@ class User:
 
 
 #####################################################
+# Room Class
+
+class Room:
+    def __init__(self, name):
+        self.name = name
+        self.members = set() # a set of sockets
+
+    def get_name(self):
+        return self.name
+
+    def add_member(self, socket):
+        if self.name not in rooms:
+            return "ERR_ROOM_NOT_EXIST"
+        if socket in self.members:
+            return "ERR_ALREADY_IN_ROOM"
+        user = find_user(socket)
+        if user:
+            self.members.add(socket)
+            return "OK_JOIN_ROOM"
+        else:
+            return "ERR_USER_NOT_EXIST"
+
+    def remove_member(self, socket):
+        if self.name not in rooms:
+            return "ERR_ROOM_NOT_EXIST"
+        if socket not in self.members:
+            return "ERR_NOT_IN_ROOM"
+        user = find_user(socket)
+        if user:
+            self.members.remove(socket)
+            return "OK_LEAVE_ROOM"
+        else:
+            return "ERR_USER_NOT_EXIST"
+
+    def get_members(self):
+        members = []
+        for socket in self.members:
+            user = find_user(socket)
+            if user:
+                members.append(user.get_name())
+
+        return members
+
+    def get_member_sockets(self):
+        return self.members
+
+# Room Class - end
+#####################################################
+
+#####################################################
 # globals
 
-users = set()  # a set of User objects
-rooms = {}  # a dictionary:  key - room name; value - set of sockets
+users = {} # a dictionary:  key - user name; value - user object
+rooms = {} # a dictionary:  key - room name; value - room object
 
 # globals - end
 #####################################################
@@ -81,50 +116,31 @@ rooms = {}  # a dictionary:  key - room name; value - set of sockets
 # dispatch functions
 
 def register(name, socket):
-    for user in users:
-        if user.get_name() == name:
-            return "ERR_USERNAME_TAKEN"
+    if name in users:
+        return "ERR_USERNAME_TAKEN"
 
-    newUser = User(name, socket)
-    users.add(newUser)
+    new_user = User(name, socket)
+    users[name] = new_user
     return "OK_REG"
 
 def create(room, socket):
     if room in rooms:
         return "ERR_ROOM_NAME_TAKEN"
 
-    user = find_user(socket)
-    if user:
-        rooms[room] = {socket}
-        print "Created room: ", room
-        return user.join_room(room)
-    else:
-        print "User not exist"
-        return "ERR_USER_NOT_EXIST"
+    new_room = Room(room) 
+    rooms[room] = new_room
+    print "Created room: ", room
+    return new_room.add_member(socket)
 
 def join(room, socket):
-    if room not in rooms:
-        return "ERR_ROOM_NOT_EXIST"
-
-    user = find_user(socket)
-    if user:
-        rooms[room].add(socket)
-        print "Joined room: ", rooms
-        return user.join_room(room)
-    else:
-        return "ERR_USER_NOT_EXIST"
+    rm = rooms[room]
+    print "Joined room: ", rooms
+    return rm.add_member(socket)
 
 def leave(room, socket):
-    if room not in rooms:
-        return "ERR_ROOM_NOT_EXIST"
-
-    user = find_user(socket)
-    if user:
-        rooms[room].remove(socket)
-        print "Left room: ", rooms
-        return user.leave_room(room)
-    else:
-        return "ERR_USER_NOT_EXIST"
+    rm = rooms[room]
+    print "Left room: ", rooms
+    return rm.remove_member(socket)
 
 def list_rooms():
     rms = ' '.join(rooms.keys())
@@ -132,13 +148,8 @@ def list_rooms():
     return ("OK_LIST " + rms)
 
 def list_members(room):
-    members = []
-    sockets = rooms[room]  # the set of sockets
-    # TODO: how to better finding user name through user socket - a Room class ??
-    for socket in sockets:
-        user = find_user(socket)
-        if user:
-            members.append(user.get_name())
+    rm = rooms[room]
+    members = rm.get_members()
 
     print "OK_MEMBERS ", ' '.join(members)
     return ("OK_MEMBERS " + ' '.join(members))
@@ -147,11 +158,15 @@ def send_message(room, socket, msg_lst):
     if room not in rooms:
         return "ERR_ROOM_NOT_EXIST"
 
-    # TODO: Check if user is in this room
     user = find_user(socket)
     if user:
-        return ("OK_MESSAGE " + user.get_name() + ' ' + room + ' ' + ' '.join(msg_lst))
-        # the msg content is needed in the return value for server to send to other users in the room
+        user_name = user.get_name()
+        rm = rooms[room]
+        if user_name in rm.get_members():
+            return ("OK_MESSAGE " + user_name + ' ' + room + ' ' + ' '.join(msg_lst))
+            # the msg content is needed in the return value for server to send to other users in the room
+        else:
+            return "ERR_NOT_IN_ROOM"
     else:
         return "ERR_USER_NOT_EXIST"
 
@@ -160,7 +175,7 @@ def quit(socket):
     
 # helper function
 def find_user(socket):
-    for user in users:
+    for user in users.itervalues():
         if user.get_socket() == socket:
             return user
     return None
@@ -169,12 +184,13 @@ def find_user(socket):
 #####################################################
 
 # data could be multiple messages queueing in the buffer, deliminated by '\n'
-# Split these messages and store in a list, dispatch each of them, store the 
-# return values in aother list, and return the list.
+# Split these messages and store them in a list, dispatch each of them,
+# store the return values in another list and return the list.
 def dispatch_multi(data, socket):
+    #TODO: change to a string ?
     ret = []
     multi_data = data.strip().split("\n")
-    print multi_data
+    #print 'multi_data:', multi_data
     for dt in multi_data:
         resp = dispatch(dt, socket)
         if resp:
@@ -186,74 +202,74 @@ def dispatch(data, socket):
     args = data.strip().split(" ")
     cmd = args[0]
     if len(args) > 1:
-        info = args[1]
+        param = args[1]
 
     if cmd == "REGISTER":
-        return register(info, socket)
+        return register(param, socket)
     elif cmd == "CREATE":
-        return create(info, socket)
+        return create(param, socket)
     elif cmd == "JOIN":
-        return join(info, socket)
+        return join(param, socket)
     elif cmd == "LEAVE":
-        return leave(info, socket)
+        return leave(param, socket)
     elif cmd == "LIST":
         return list_rooms()
     elif cmd == "MEMBERS":
-        return list_members(info)
+        return list_members(param)
     elif cmd == "MESSAGE":
         msg_lst = args[2:]
-        return send_message(info, socket, msg_lst)
+        return send_message(param, socket, msg_lst)
     elif cmd == "QUIT":
         return quit(socket)
     elif cmd == "PONG":
-        print 'dispatch', cmd
         # if receive a 'PONG' from some user, mark it as active
         user = find_user(socket)
         user.mark_active()
         pass
     else:
-        print 'dispatch', cmd
         return "ERR_INVALID"
     
-def ping_clients():
-    # iterate all clients and send 'PING' to everyone.
-    for user in users:
-        sckt = user.get_socket()
-        sckt.send('PING\n')
-        print 'ping_clients'
+def ping_clients(seconds):
+    while True:
+        # iterate all clients and send 'PING' to everyone.
+	# TODO: dictionary changed size during iteration
+        for user in users.itervalues():
+            sckt = user.get_socket()
+            sckt.send('PING\n')
 
-        #resp = sckt.recv(size)
-    sleep(1)
-    for user in users:
-        if user.check_active() == False:
-            # if any user is inactive, it means no 'PONG' was received 
-            # last round from its socket. so client crashed
-            # handle client left situation 
-            handle_client_leave(user)               
-        # mark all the users as inactive for next round of PING
-        user.mark_inactive()
+	# give client 1 second to respond, if no response in 1 sec, consider the client as crashed
+	sleep(1)
+        for user in users.itervalues():
+	    # if no 'PONG' was received, the client crashes, marked as inactive
+	    if not user.is_active():
+		# handle client crash
+		handle_client_leave(user)               
+	    # mark all the users as inactive for next round of PING
+	    user.mark_inactive()
 
-# this function is called when a user sends "QUIT", 
-# or when a client is crashed 
+        sleep(seconds)
+
+# called when a client sends "QUIT" or crashes
 def handle_client_leave(user):
     if user:
         sckt = user.get_socket()
         sckt.close()
-        # remove client from user list
-        users.remove(user)
         
-        rms = user.get_rooms()
         name = user.get_name()
-        for room in rms:
-            sckts = rooms[room]
+        rms = user.get_rooms()
+        for rm in rms:
+            room = rooms[rm]
             # remove the client from this room
-            sckts.remove(sckt)
+            room.remove_member(sckt)
 
             # send message to other members of this room
+            sckts = room.get_member_sockets()
             for s in sckts:
+                print 'NOTICE user ' + name + ' has left\n'
                 s.send('NOTICE user ' + name + ' has left\n')
 
-
+        # remove client from user list
+        del users[name]
 #####################################################
 # main function
 
@@ -268,31 +284,36 @@ def main():
     server.bind((host,port))
     server.listen(backlog)
     print "Server is running on port 8080"
+
+    thread = Thread(target = ping_clients, args = (1, ))
+    thread.start()
+
     inputs = [server]
     running = 1
     while running:
-        ping_clients()
+        #ping_clients()
         readable, writable, exceptional = select.select(inputs,[],[])
         for s in readable:
             # if the socket is server socket
             # accept new client connection and add it to the connection list
             if s is server:
                 connection, client_address = s.accept()
-                #connection.setblocking(0)
+                connection.setblocking(0)
                 inputs.append(connection)
 
             # if the socket is client socket
             # read data from the socket and process them
             else:
                 try:
-                    # used try except because if client closes the connection, 
+                    # use `try` `except` because if client closes the connection, 
                     # s.recv() here will cause error.
-
                     data = s.recv(size)
                     responses = dispatch_multi(data, s)
-                    #if response:
+		    #print 'responses:', responses
+
                     for response in responses:
                         resp = response.split(' ')
+			#print 'resp:', resp
                         status = resp[0]
                         # TODO: extract to a function
                         if status == "OK_MESSAGE":
@@ -304,22 +325,18 @@ def main():
 
                             #user = resp[1]
                             room = resp[2]
-                            print data
-                            for sckt in rooms[room]:
-                                #sckt.send(data + ' ' + user + '\n')
+                            print 'data:', data
+                            for sckt in rooms[room].get_member_sockets():
                                 sckt.send('MESSAGE ' + to_send + '\n')
+			# if the client intentionally disconnect from server
                         elif status == "OK_QUIT":
                             s.send(status + "\n")
                             handle_client_leave(find_user(s))
                             inputs.remove(s)
                             # TODO Inform related users about this user's quit
                         else:
-                            print response
-                            s.send(response +"\n")
-                    # if no valid response for this request
-                    # continue to the next client
-                    # else:
-                    #    continue
+                            s.send(response + "\n")
+		# if the socket is broken
                 except socket.error:
                     handle_client_leave(find_user(s))
                     inputs.remove(s)
